1493fd67db8334482ef6b5863a8c471710f50b8a
ericgold <egold05@gmail.com>
2016-03-26 19:11:31 -0700

    search and overlay
    added search based on Duckett book
    added left and right arrows to overlay

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,14 +1,22 @@
 /* Create an overlay with image container, caption,
 left arrow, and right arrow */
 
-var $overlay = $('<div id="overlay"></div>');
+var $overlay = $("<div id='overlay'></div>");
 var $image = $("<img>");
-var $caption = $("<p></p>");
+var $caption = $("<p id='caption'></p>");
+var $leftArrow = $("<p id='left-arrow'>&#10094</p>");
+var $rightArrow = $("<p id='right-arrow'>&#10095</p>");
+
+var $searchField = $("input.search");
+var $thumbnails = $(".thumbnail img");
+var cache = [];
 
 // Add left arrow to overlay
+$overlay.append($leftArrow);
 // Add image to overlay
 $overlay.append($image);
 // Add right arrow to overlay
+$overlay.append($rightArrow);
 // Add caption to overlay
 $overlay.append($caption);
 // Add overlay to document
@@ -28,30 +36,61 @@ $(".thumbnail a").click(function(event){
 	$caption.text(captionText);
 });
 
+// On right arrow click OR right arrow keypress
+	// Advance to next photo
+// On left arrow click OR left arrow keypress
+	// Go back to last photo
+
 // On overlay click
 $overlay.click(function(){
 	// Hide overlay
 	$overlay.hide();
 });
-// On right arrow click OR right arrow keypress
-	// Advance to next photo
-// On left arrow click OR left arrow keypress
-	// Go back to last photo
+
 
 
 /* Enable search, dynamically hiding photos whose
 captions do not include the inputted string and 
 updating for each character entered */
 
+
+
 // When search input is emtpy, show all images
 // On character input (keyup)
-	// Capture input string
-	// Compare input string to captions
-	// For each caption
+
+
+
+
+// Add alt text of each gallery image to cache array
+$thumbnails.each(function() {
+	cache.push({
+		element: this,
+		text: this.alt.trim().toLowerCase()
+	});
+});
+
+// Compare input string to captions
+// For each caption
 		// if the caption contains string
 			// Show corresponding image (animated)
 		// if caption does not contain string
 			// Hide corresponding image	(animated)
+			
+var filter = function() {
+	var query = this.value.trim().toLowerCase();
+
+	cache.forEach(function(img) {
+		var index = 0;
+		if (query) {
+			index = img.text.indexOf(query);
+			img.element.style.display = index === -1 ? 'none' : '';
+		};
+	});
+}
+
+$searchField.keyup(filter);
+
+
 
 /* Support additional media types 
 like YouTube videos */
